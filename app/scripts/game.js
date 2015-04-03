bc8e98dee5cbad5b8c6e27b683cfbf3f2b96d16d
bjorgheidur <bjorgheidur12@ru.is>
2015-04-03 15:46:06 +0000

    jgj

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -13,6 +13,11 @@ window.Game = (function () {
 
 		// Cache a bound onFrame since we need it each frame.
 		this.onFrame = this.onFrame.bind(this);
+
+		this.backgroundMusic = new buzz.sound('../app/sounds/background_music.mp3', {
+			loop: true,
+			autoplay: true
+		});
 	};
 
 	/**
