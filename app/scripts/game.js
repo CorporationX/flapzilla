734510261b523ee185c24783959c97c117b1fea6
bjorgheidur <bjorgheidur12@ru.is>
2015-04-03 18:24:47 +0000

    gasdgas

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -9,6 +9,7 @@ window.Game = (function () {
 	var Game = function (el) {
 		this.el = el;
 		this.player = new window.Player(this.el.find('.Player'), this);
+		this.score = 0;
 		this.isPlaying = false;
 
 		// Cache a bound onFrame since we need it each frame.
@@ -20,6 +21,8 @@ window.Game = (function () {
 		});
 		this.crashSound = new buzz.sound('/app/sounds/crash.mp3');
 		this.wingFlapSound = new buzz.sound('/app/sounds/flap.wav');
+		$('.Score-text').text(this.score);
+
 	};
 
 	/**
@@ -61,7 +64,9 @@ window.Game = (function () {
 	 */
 	Game.prototype.reset = function () {
 		this.player.reset();
+		this.score = 0;
 		$('.Start-text').css('display', 'block');
+		$('.Score-top').css('display', 'block');
 	};
 
 	/**
@@ -75,6 +80,8 @@ window.Game = (function () {
 
 		// Should be refactored into a Scoreboard class.
 		var that = this;
+
+		$('.Score-top').css('display', 'none');
 		var scoreboardEl = this.el.find('.Scoreboard');
 		scoreboardEl
 			.addClass('is-visible')
@@ -83,6 +90,11 @@ window.Game = (function () {
 				scoreboardEl.removeClass('is-visible');
 				that.start();
 			});
+	};
+
+	Game.prototype.updateScore = function () {
+		this.score++;
+		$('.Score-text').text(this.score);
 	};
 
 	/**
